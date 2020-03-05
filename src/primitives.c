c700e2890cea578dc580169e585175f54cb6d49e
Gérard Milmeister <gemi@bluewin.ch>
2020-03-05 13:35:34 +0100

    RC primitive

--- src/primitives.c
+++ src/primitives.c
@@ -51,6 +51,149 @@ static CHAR** make_fargs(ARGS* args, int* fnargs, int start)
     return fargs;
 }
 
+/*
+ * Primitives for input and output control (PS, RS, CM, RC).
+ */
+
+static int prim_ps(TRAC* trac, ARGS* args)
+{
+    if (args->n > 1) {
+        CHAR* s = &args->buf->buf[args->pos[1]];
+        int len = strlen(c(s));
+        ansi_fg(trac->fd_out, OUTPUT_COLOR);
+        io_out(trac->fd_out, s, len);
+        ansi_reset(trac->fd_out);
+    }
+    return 0;
+}
+
+static int prim_rs(TRAC* trac, ARGS* args)
+{
+    ibuf->len = 0;
+    io_in(trac->fd_in, trac->fd_out,
+          trac->meta_char, trac->dump_char, trac->delete_char,
+          ibuf);
+    value(args->to, trac, ibuf->buf, ibuf->len);
+    return 0;
+}
+
+static int prim_cm(TRAC* trac, ARGS* args)
+{
+    CHAR* s = get_arg(args, 1);
+    if (strlen(c(s)) > 0) {
+        CHAR meta = s[0];
+        if (meta != '#' && meta != '(' && meta != ',') {
+            trac->meta_char = meta;
+        }
+    }
+    return 0;
+}
+
+static int prim_rc(TRAC* trac, ARGS* args)
+{
+    CHAR c = io_char(trac->fd_in);
+    value(args->to, trac, &c, 1);
+    return 0;
+}
+
+/*
+ * Primitives for string storage and deletion (DS, DD, DA).
+ */
+
+static int prim_ds(TRAC* trac, ARGS* args)
+{
+    CHAR* name = get_arg(args, 1);
+    CHAR* s = get_arg(args, 2);
+    form_define(trac->forms, name, s, strlen(c(s)));
+    return 0;
+}
+
+static int prim_dd(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_da(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+/*
+ * Primitive for the creation of text macros (SS).
+ */
+
+static int prim_ss(TRAC* trac, ARGS* args)
+{
+    CHAR* name = get_arg(args, 1);
+    form* f = form_lookup(trac->forms, name);
+    if (f) {
+        int fnargs = 0;
+        CHAR** fargs = make_fargs(args, &fnargs, 2);
+        form_ss(f, fargs, fnargs);
+        free(fargs);
+    }
+    return 0;
+}
+
+/*
+ * Call primitive (CL).
+ */
+
+static int prim_cl(TRAC* trac, ARGS* args)
+{
+    CHAR* name = get_arg(args, 1);
+    form* f = form_lookup(trac->forms, name);
+    if (f) {
+        int fnargs = 0;
+        CHAR** fargs = make_fargs(args, &fnargs, 2);
+        CHAR* s = form_get(f, fargs, fnargs);
+        free(fargs);
+        value(args->to, trac, s, strlen(c(s)));
+        free(s);
+    }
+    return 0;
+}
+
+/*
+ * Partial call primitives (CR, CC, CS, CN, IN).
+ */
+
+static int prim_cr(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_cc(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_cs(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_cn(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_in(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+/*
+ * Arithmetic Primitives (AD, SU, ML, DV).
+ */
+
 static CHAR* prim_arith_get_args(TRAC* trac, ARGS* args, long* n1, long *n2) {
     CHAR* a1 = get_arg(args, 1);
     CHAR* a2 = get_arg(args, 2);
@@ -120,69 +263,124 @@ static int prim_dv(TRAC* trac, ARGS* args)
     return 0;
 }
 
-static int prim_cl(TRAC* trac, ARGS* args)
+/*
+ * Boolean primitives (BU, BI, BC, BR, BS).
+ */
+
+static int prim_bu(TRAC* trac, ARGS* args)
 {
-    CHAR* name = get_arg(args, 1);
-    form* f = form_lookup(trac->forms, name);
-    if (f) {
-        int fnargs = 0;
-        CHAR** fargs = make_fargs(args, &fnargs, 2);
-        CHAR* s = form_get(f, fargs, fnargs);
-        free(fargs);
-        value(args->to, trac, s, strlen(c(s)));
-        free(s);
-    }
+    // TODO
     return 0;
 }
 
-static int prim_ds(TRAC* trac, ARGS* args)
+static int prim_bi(TRAC* trac, ARGS* args)
 {
-    CHAR* name = get_arg(args, 1);
-    CHAR* s = get_arg(args, 2);
-    form_define(trac->forms, name, s, strlen(c(s)));
+    // TODO
     return 0;
 }
+
+static int prim_bc(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_br(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_bs(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+/*
+ * Decision primitives (EQ, GR).
+ */
+
+static int prim_eq(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_gr(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+/*
+ * Auxiliary storage primitives (SB, FB, EB).
+ */
+
+static int prim_sb(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_fb(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_eb(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+/*
+ * Diagnostic primitives (LN, PF, TN, TF).
+ */
+
+static int prim_ln(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_pf(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_tn(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+static int prim_tf(TRAC* trac, ARGS* args)
+{
+    // TODO
+    return 0;
+}
+
+/*
+ * Housekeeping primitives: HL, MO
+ */
 
 static int prim_hl(TRAC* trac, ARGS* args)
 {
     exit(0);
 }
 
-static int prim_ps(TRAC* trac, ARGS* args)
+static int prim_mo(TRAC* trac, ARGS* args)
 {
-    if (args->n > 1) {
-        CHAR* s = &args->buf->buf[args->pos[1]];
-        int len = strlen(c(s));
-        ansi_fg(trac->fd_out, OUTPUT_COLOR);
-        io_out(trac->fd_out, s, len);
-        ansi_reset(trac->fd_out);
-    }
+    // TODO
     return 0;
 }
 
-static int prim_rs(TRAC* trac, ARGS* args)
-{
-    ibuf->len = 0;
-    io_in(trac->fd_in, trac->fd_out,
-          trac->meta_char, trac->dump_char, trac->delete_char,
-          ibuf);
-    value(args->to, trac, ibuf->buf, ibuf->len);
-    return 0;
-}
-
-static int prim_ss(TRAC* trac, ARGS* args)
-{
-    CHAR* name = get_arg(args, 1);
-    form* f = form_lookup(trac->forms, name);
-    if (f) {
-        int fnargs = 0;
-        CHAR** fargs = make_fargs(args, &fnargs, 2);
-        form_ss(f, fargs, fnargs);
-        free(fargs);
-    }
-    return 0;
-}
-
+/*
+ * Default Call.
+ */
 static int prim_default(TRAC* trac, ARGS* args)
 {
     CHAR* name = get_arg(args, 0);
@@ -232,15 +430,40 @@ void primitives_init()
 {
     prims = calloc(MAX_PRIM, sizeof(primitive));
     reg_prim("AD", prim_ad);
+    reg_prim("BC", prim_bc);
+    reg_prim("BI", prim_bi);
+    reg_prim("BR", prim_br);
+    reg_prim("BS", prim_bs);
+    reg_prim("BU", prim_bu);
+    reg_prim("CC", prim_cc);
     reg_prim("CL", prim_cl);
+    reg_prim("CM", prim_cm);
+    reg_prim("CN", prim_cn);
+    reg_prim("CR", prim_cr);
+    reg_prim("CS", prim_cs);
+    reg_prim("DA", prim_da);
+    reg_prim("DD", prim_dd);
     reg_prim("DS", prim_ds);
     reg_prim("DV", prim_dv);
+    reg_prim("EB", prim_eb);
+    reg_prim("EQ", prim_eq);
+    reg_prim("FB", prim_fb);
+    reg_prim("GR", prim_gr);
     reg_prim("HL", prim_hl);
+    reg_prim("IN", prim_in);
+    reg_prim("LN", prim_ln);
     reg_prim("ML", prim_ml);
+    reg_prim("MO", prim_mo);
+    reg_prim("PF", prim_pf);
     reg_prim("PS", prim_ps);
+    reg_prim("PS", prim_ps);
+    reg_prim("RC", prim_rc);
     reg_prim("RS", prim_rs);
+    reg_prim("SB", prim_sb);
     reg_prim("SS", prim_ss);
     reg_prim("SU", prim_su);
+    reg_prim("TF", prim_tf);
+    reg_prim("TN", prim_tn);
     default_prim.name = C("");
     default_prim.fun = prim_default;
 }
